e4b4bf3222ea73c117eccf8e97dd4b722eaf5ea8
And-Bond <aloxnesir@gmail.com>
2024-10-21 20:22:44 +0300

    initial commit

---
+++ server.js
@@ -0,0 +1,37 @@
+'use strict';
+
+const Hapi = require('@hapi/hapi');
+const dotenv = require('dotenv');
+dotenv.config()
+
+const { API_HOST } = process.env
+
+const init = async () => {
+    // Create a new Hapi server instance
+    const server = Hapi.server({
+        port: API_HOST,        // Set the port
+        host: 'localhost', // Set the host
+    });
+
+    // Basic route: Responds with "Hello, Hapi!" when accessed via GET
+    server.route({
+        method: 'GET',
+        path: '/',
+        handler: (request, h) => {
+            return 'GN Backed is working good';
+        }
+    });
+
+    // Start the server
+    await server.start();
+    console.log('Server running on %s', server.info.uri);
+};
+
+// Handle any errors when starting the server
+process.on('unhandledRejection', (err) => {
+    console.log(err);
+    process.exit(1);
+});
+
+// Initialize the server
+init()
