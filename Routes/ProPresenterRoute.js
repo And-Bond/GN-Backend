e4ef25b1ab30f5769f94d67002c8e85341847588
And-Bond <aloxnesir@gmail.com>
2025-03-09 18:19:39 +0200

    created test propresenter route

---
+++ Routes/ProPresenterRoute.js
@@ -0,0 +1,27 @@
+const dotenv = require('dotenv')
+dotenv.config()
+
+module.exports = [
+  {
+      method: 'POST',
+      path: '/pro_webhook',
+      handler: async(req,h) => {
+          try {
+            let payload = req.payload
+            if(payload.ngrok_url){
+              process.env.PRO_PRESENTER_PATH = payload.ngrok_url
+              console.log('ProPresenter Webhook is set to', process.env.PRO_PRESENTER_PATH)
+              return { msg: 'Success!, Set to ' + process.env.PRO_PRESENTER_PATH }
+            }
+            return { message: 'Failed to set ngrok url'}
+          } catch (error) {
+              console.log('ROUTE ERROR:',error)
+              return { data: false }
+          }
+
+      },
+      options: {
+
+      }
+  },
+]
