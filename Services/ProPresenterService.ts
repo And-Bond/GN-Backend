d085af5388b95e968c099ef0089bdb32ce60df19
And-Bond <aloxnesir@gmail.com>
2025-03-22 16:38:18 +0200

    extended propresenter functional

--- Services/ProPresenterService.ts
+++ Services/ProPresenterService.ts
@@ -1,7 +1,7 @@
 
 import axios from "axios";
 // Type imports
-import type { Group, Library, Playlist, PlaylistItem, LibraryItem } from "types/pro-presenter.js";
+import type { Group, Library, Playlist, PlaylistItem, LibraryPlaylist, Presentation, Prop, PropCollection, StageScreen, StageLayout } from "types/pro-presenter.js";
 
 const path = 'http://192.168.1.172:3005/v1/'
 
@@ -9,33 +9,69 @@ const api = axios.create({
     baseURL: path,
 });
 
-const tiggerNextSlide = async() => api.get('trigger/next');
+/**
+ * UUID representation of item
+ * for example: 3C39C433-5C18-4F51-B357-55BB870227C4
+ */
+type UUID = string
 
-const tiggerPreviousSlide = async() => api.get('trigger/previous');
+/**
+ * UUID, name or index of item
+ */
+type ID = string
 
-const triggerNextMedia = async() => api.get('trigger/media/previous');
-
-const triggerPreviousMedia = async() => api.get('trigger/media/previous');
-
-const triggerNextAudio = async() => api.get('trigger/audio/previous');
-
-const triggerPreviousAudio = async() => api.get('trigger/audio/previous');
-
-const trigggerSlide = async(uuid: string, index: string) => api.get(`presentation/${uuid}/${index}/trigger`);
-
-const getAllGroups = async(): Promise<Group[]> => api.get('groups');
+// Libraries
 
 const getAllLibraries = async(): Promise<Library[]> => api.get('libraries');
 
-const getLibraryItems = async(id: string): Promise<LibraryItem[]> => api.get('libraries/' + id);
+// Playlists
 
 const getAllPlaylists = async(): Promise<Playlist[]> => api.get('playlists');
 
-const getPlaylistItems = async(id: string): Promise<PlaylistItem[]> => api.get('playlists/' + id);
+const getLibraryPlaylists = async(id: ID): Promise<LibraryPlaylist[]> => api.get('libraries/' + id);
+
+const getPlaylistItems = async(id: ID): Promise<Playlist['id'] & { items: PlaylistItem[] } > => api.get('playlists/' + id);
+
+// Presentations
+
+const getPresentationById = async(uuid: UUID): Promise<{ presentation: Presentation }> => api.get('presentation' + uuid)
 
 const getActivePresentation = async() => api.get('presentation/active');
 
-module.exports = {
+// Props
+
+const getAllPropCollections = async(): Promise<{ collections: PropCollection[] }> => api.get('prop_collections')
+
+const getAllProps = async(): Promise<Prop[]> => api.get('props');
+
+// Groups
+
+const getAllGroups = async(): Promise<Group[]> => api.get('groups');
+
+// Stage Screens/Layout
+
+const getAllStageScreens = async(): Promise<StageScreen[]> => api.get('stage/screens')
+
+const getAllStageLayouts = async(): Promise<StageLayout[]> => api.get('stage/layouts')
+
+// Triggers
+
+const tiggerNextSlide = async(): Promise<void> => api.get('trigger/next');
+
+const tiggerPreviousSlide = async(): Promise<void> => api.get('trigger/previous');
+
+const triggerNextMedia = async(): Promise<void> => api.get('trigger/media/previous');
+
+const triggerPreviousMedia = async(): Promise<void> => api.get('trigger/media/previous');
+
+const triggerNextAudio = async(): Promise<void> => api.get('trigger/audio/previous');
+
+const triggerPreviousAudio = async(): Promise<void> => api.get('trigger/audio/previous');
+
+const trigggerSlide = async(uuid: UUID, index: string): Promise<void> => api.get(`presentation/${uuid}/${index}/trigger`);
+
+
+export default {
     tiggerNextSlide,
     tiggerPreviousSlide,
     triggerNextMedia,
@@ -48,5 +84,10 @@ module.exports = {
     getAllLibraries,
     getAllPlaylists,
     getPlaylistItems,
-    getLibraryItems
+    getLibraryPlaylists,
+    getPresentationById,
+    getAllPropCollections,
+    getAllProps,
+    getAllStageScreens,
+    getAllStageLayouts
 };
