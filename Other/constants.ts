913b2917c5aa51acec9b4fb9d79dcd6a024512a2
Гавриш Олександр <sashawwe21@gmail.com>
2025-10-24 23:22:32 +0300

    add new contact

--- Other/constants.ts
+++ Other/constants.ts
@@ -23,7 +23,8 @@ export default {
     TgPhoneUsers: [
         "380663309198", // Alex Havrysh
         "380676588536", // Valua Kolomiec
-        "380502591405" // Alex Datsyk
+        "380502591405", // Alex Datsyk
+        "380931866487" // Good news contact
     ],
     TgPhoneUsersDev: [
         "380663309198", // Alex Havrysh
